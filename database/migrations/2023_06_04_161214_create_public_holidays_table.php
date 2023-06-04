76145842d85fc0e7230c3a44887a90c0ab697ec1
work <temogogitidze@redberry.ge>
2023-06-04 20:19:17 +0400

    feat: add public holiday model

--- database/migrations/2023_06_04_161214_create_public_holidays_table.php
+++ database/migrations/2023_06_04_161214_create_public_holidays_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('calendar_events', function (Blueprint $table) {
+        Schema::create('public_holidays', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->date('start_date');
@@ -19,6 +19,6 @@ return new class extends Migration
     }
     public function down(): void
     {
-        Schema::dropIfExists('calendar_events');
+        Schema::dropIfExists('public_holidays');
     }
 };
