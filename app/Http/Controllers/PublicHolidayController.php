4fcb8259a62f042175257470564fc009392e0eb0
work <temogogitidze@redberry.ge>
2023-06-04 20:51:25 +0400

    feat: add custom request, api endpoint and controller for public holidays

---
+++ app/Http/Controllers/PublicHolidayController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PublicHolidayController extends Controller
+{
+    public function index()
+    {
+        
+    }
+}
